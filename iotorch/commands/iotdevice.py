99fd7adf8b57ee2a8c531a29b9541f2b35a22156
juanmagal <juanma.galmes@gmail.com>
2019-02-10 02:22:29 +0100

    Adding getters to iotdevice

--- iotorch/commands/iotdevice.py
+++ iotorch/commands/iotdevice.py
@@ -1,7 +1,7 @@
 """
 iotorch iotdevice
 
-  Usage:  iotorch iotdevice [create|delete] --name=<name>   
+  Usage:  iotorch iotdevice [create|delete|get|list] [--name=<name>] [--configfile=<name>]   
 
 """
 
@@ -10,6 +10,10 @@ from json import dumps
 from .base import Base
 
 from docopt import docopt
+
+import toml
+
+import os
 
 class Iotdevice(Base):
     """The IoT Device command."""
@@ -22,6 +26,35 @@ class Iotdevice(Base):
         print('Deleting IoT Device:',self.options['--name'])
         print('You supplied the following options:', dumps(self.options, indent=2, sort_keys=True))
 
+    def get(self):
+        config_path = self.options['--configfile']
+
+        if (not config_path):
+           config_path='./iotorch.toml'
+
+        if not os.path.exists(config_path):
+           print('Nothing to get')
+        else:
+           with open(config_path) as f:
+               config = toml.load(f)
+               devices = config['iotdevice']
+               print(devices[self.options['--name']])
+
+    def list(self):
+
+        config_path = self.options['--configfile']
+
+        if (not config_path):
+           config_path='./iotorch.toml'
+
+        if not os.path.exists(config_path):
+           print('Nothing to list')
+        else:
+           with open(config_path) as f:
+               config = toml.load(f)
+               devices = config['iotdevice']
+               print (list(devices.keys()))
+
     def run(self):
 
         options = docopt(__doc__)
@@ -32,6 +65,12 @@ class Iotdevice(Base):
         elif options['delete']:
             self.options=options
             self.delete()
+        elif options['get']:
+            self.options=options
+            self.get()
+        elif options['list']:
+            self.options=options
+            self.list()
         else:
             print("Option not implemented")
             raise NotImplementedError('Option not implemented')
--- tests/commands/test_iotdevice.py
+++ tests/commands/test_iotdevice.py
@@ -22,3 +22,31 @@ class TestIoTDevice(TestCase):
         output = popen(['iotorch', 'iotdevice', operation, '--name='+name], stdout=PIPE).communicate()[0]
         self.assertTrue(text.encode('utf-8') in output)
 
+    def test_returns_iotdevice_get(self):
+        name='test'
+        operation='get'
+        configfile='./tests/conf/iotorch.toml'
+        output = popen(['iotorch', 'iotdevice', operation, '--name='+name, '--configfile='+configfile], stdout=PIPE).communicate()[0]
+        self.assertTrue(name.encode('utf-8') in output)
+
+    def test_returns_iotdevice_get_file_does_not_exit(self):
+        name='test'
+        operation='get'
+        text='Nothing to get'
+        output = popen(['iotorch', 'iotdevice', operation, '--name='+name], stdout=PIPE).communicate()[0]
+        self.assertTrue(text.encode('utf-8') in output)
+
+    def test_returns_iotdevice_list(self):
+        name='test'
+        operation='list'
+        configfile='./tests/conf/iotorch.toml'
+        output = popen(['iotorch', 'iotdevice', operation, '--configfile='+configfile], stdout=PIPE).communicate()[0]
+        self.assertTrue(name.encode('utf-8') in output)
+
+    def test_returns_iotdevice_list_file_does_not_exist(self):
+        name='test'
+        operation='list'
+        text='Nothing to list'
+        output = popen(['iotorch', 'iotdevice', operation], stdout=PIPE).communicate()[0]
+        self.assertTrue(text.encode('utf-8') in output)
+
